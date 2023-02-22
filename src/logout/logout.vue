f4fe775f8c82d60bec2435567ad395b40af0b684
zhaoge7220 <10527202+zhaoge7220@user.noreply.gitee.com>
2023-02-22 19:27:48 +0800

    asd

--- src/logout/logout.vue
+++ src/logout/logout.vue
@@ -1,14 +1,1 @@
-<template>
-  <div>{{ new1 }}</div>
-</template>
-<script>
-export default {
-  data() {
-    return {
-      tetssss: "123",
-      asd: "123",
-      new: "asd",
-    };
-  },
-};
-</script>
+
