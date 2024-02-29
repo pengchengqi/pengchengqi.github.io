3f718ef15ecdb2916320dfe6ad845ae7ae3c9dcc
hcnhcn012 <hcnhcn012@126.com>
2024-02-29 23:01:33 +0800

    init commit

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  lintOnSave: false,
-  transpileDependencies: true,
-  publicPath: '',//github仓库名
-  assetsDir: '',//同上，否则访问不到静态资源
-  outputDir:"dist"
-})
